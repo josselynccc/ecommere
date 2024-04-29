cfec91c2f0ff1f6e10552b6591504e135fcc342b
Josselyn Cortez <cristheljosselyn1@gmail.com>
2024-04-29 12:25:25 -0500

    Colcoar archivo PDF en mi componente cv

--- src/components/pages/Cv.jsx
+++ src/components/pages/Cv.jsx
@@ -1,20 +1,20 @@
-// const Cv = () =>{    
+const Cv = () =>{    
 
-//     return<>
-//     <iframe src="https://drive.google.com/file/d/1pRXOAmIjyj-DAPPJfYbXqvm4cFYQcmps/preview" width="500" height="700"></iframe>
+    return<>
+    <iframe src="https://drive.google.com/file/d/1pRXOAmIjyj-DAPPJfYbXqvm4cFYQcmps/preview" width="500" height="700"></iframe>
 
-//     </>
-// }
-// export default Cv
-
-import curriculumPdf from '../../assets/curriculum.pdf';
-
-const Cv = () => {
-    return (
-        <object data={curriculumPdf} type="application/pdf" width="100%" height="100%">
-            <p>El visor de PDF no es compatible con su navegador. Puede descargar el PDF <a href={curriculumPdf}>aquí</a>.</p>
-        </object>
-    );
+    </>
 }
+export default Cv
 
-export default Cv;
+// import curriculumPdf from '../../assets/curriculum.pdf';
+
+// const Cv = () => {
+//     return (
+//         <object data={curriculumPdf} type="application/pdf" width="100%" height="100%">
+//             <p>El visor de PDF no es compatible con su navegador. Puede descargar el PDF <a href={curriculumPdf}>aquí</a>.</p>
+//         </object>
+//     );
+// }
+
+// export default Cv;
